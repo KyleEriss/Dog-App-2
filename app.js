d19a24cd07783034ac18e3052a369ac34501b249
Kyle Eriss <kyle.eriss@gmail.com>
2020-10-01 01:17:44 -0400

    final revision

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 function getDogImage() {
-    let userInput = document.getElementById("dogBreed");
-    let dynamicUrl = "https://dog.ceo/api/breed/"${userInput}"/images/random";
-    fetch(dynamicUrl)
-    .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
-    .catch(error => alert("Something went wrong. Try again later."));
+    let rawUserInput = document.getElementById("dogBreed").value;
+    let userInput = rawUserInput.toLowerCase();
+    let userInputLength = countWords(userInput);
+    if (userInputLength === 1) {
+        let dynamicUrl = `https://dog.ceo/api/breed/${userInput}/images/random`;
+        
+        // Check if file found
+        fetch(dynamicUrl)
+        .then(handleErrors)
+        .then(response => console.log("ok") )
+        .catch(error => alert("Sorry, breed not found. Please check spelling."));
+        
+        // Resume program if file found
+        fetch(dynamicUrl)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert("Something went wrong. Try again later."));
+    }
+    else if (userInputLength === 2) {
+        let userInputArray = userInput.split(" ");
+        let subBreed = userInputArray[0];
+        let breed = userInputArray[1]
+        let dynamicUrl = `https://dog.ceo/api/breed/${breed}/${subBreed}/images/random`;
+        
+        // Check if file found
+        fetch(dynamicUrl)
+        .then(handleErrors)
+        .then(response => console.log("ok") )
+        .catch(error => alert("Sorry, breed not found. Please check spelling."));
+
+        // Resume program if file found
+        fetch(dynamicUrl)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert("Something went wrong. Try again later."));
+    }
 }
+
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+    function countWords(userInput) { 
+    return userInput.split(" ").length;
+  }
+  
     
 function displayResults(responseJson) {
     console.log(responseJson);
@@ -17,6 +59,9 @@ function displayResults(responseJson) {
 function watchForm() {
     $("form").submit(event => {
         event.preventDefault();
+        if (document.getElementById("dogBreed").value === 0) {
+            return alert("Please enter dog breed");
+        }
         $(".results-img").empty();
         getDogImage();
     });
